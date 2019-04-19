b35462671577adfd065e13554b05655743d5ebca
trickstival <trick_stival@hotmail.com>
2019-04-19 20:04:18 -0300

    Configuring axios and removing Request.js

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,21 @@
+import axios from 'axios/index'
+
 export default function Router (routeMap) {
-    return {
-        
+    this.server = ''
+}
+
+Router.prototype.setServer = (serverUrl) => {
+    this.serverUrl = serverUrl
+}
+
+const methods = ['get', 'post', 'put', 'patch', 'delete']
+
+for (const method of methods) {
+    Router.prototype[method] = () => {
+        return axios({
+            method,
+            dataType: 'json',
+            url: this.serverUrl
+        })
     }
 }
